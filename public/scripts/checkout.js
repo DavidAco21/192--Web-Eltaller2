2d4523394d64b290a93e3112f3981846f8ff572b
DavidAco21 <davidgar_21@hotmail.com>
2019-11-27 23:52:52 -0500

    listo js para enviar pedido a base de datos

--- public/scripts/checkout.js
+++ public/scripts/checkout.js
@@ -5,7 +5,18 @@ window.addEventListener('load', function() {
     add__btn.addEventListener('submit', function(event){
         event.preventDefault();
         var order = new FormData(add__btn);
-        
+        var data = new URLSearchParams(order);
+        console.log(data, order);
+
+        var promise = fetch('/checkout/orders', {method: 'POST', body: data});
+        promise
+            .then(function(response){
+                console.log(response);
+                return response.json();
+            })
+            .then (function(data){
+                console.log(data);
+            });
 
 
     });
